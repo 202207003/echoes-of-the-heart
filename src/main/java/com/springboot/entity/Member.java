314a5a38f3390cefc1ac7786e5a5e81936080865
limwanhee <applespeed555@gmail.com>
2026-06-09 14:44:28 +0900

    회원가입, 로그인, 아이디 중복 체크 추가

--- src/main/java/com/springboot/entity/Member.java
+++ src/main/java/com/springboot/entity/Member.java
@@ -1,9 +1,17 @@
 package com.springboot.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "member")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Member {
 
     @Id
